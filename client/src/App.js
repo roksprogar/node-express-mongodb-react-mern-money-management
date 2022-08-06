a6c6fe3f0a2c94c489dc791c9f0c87a710009530
Rok Sprogar <rok.sprogar@gmail.com>
2022-08-06 15:49:59 +0200

    added react libs and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Personal money management</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }

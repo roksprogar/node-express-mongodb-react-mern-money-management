e37550946c6726830656c53b5e560018550c93ad
Rok Sprogar <rok.sprogar@gmail.com>
2022-08-07 21:38:01 +0200

    hroku config

--- server/server.js
+++ server/server.js
@@ -4,9 +4,18 @@ const express = require('express');
 const dbConnect = require('./dbConnect');
 const userRoutes = require('./routes/userRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
+const path = require('path');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('client/build'));
+
+  app.get('*', (res, req) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
